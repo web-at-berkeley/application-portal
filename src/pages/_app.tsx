dbaed4e47b207d162efeaaace5f0ba5c40b46029
Anish Shanbhag <anish.shanbhag@yahoo.com>
2022-09-29 04:48:44 -0700

    Basic Convex setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,36 +13,45 @@ import {
 import NextNProgress from "nextjs-progressbar";
 import { ErrorBoundary } from "react-error-boundary";
 import { WarningTwoIcon } from "@chakra-ui/icons";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithAuth0 } from "convex/react-auth0";
 
+import convexConfig from "../../convex.json";
+import clientConfig from "../../convex/_generated/clientConfig";
 import { theme } from "../utils/theme";
+
+const convex = new ConvexReactClient(clientConfig);
+const authInfo = convexConfig.authInfo[0];
 
 export default function WeeklyApp({ Component, pageProps }: AppProps) {
   const { onClose } = useDisclosure();
   return (
-    <ChakraProvider theme={theme}>
-      <ErrorBoundary
-        FallbackComponent={() => (
-          <Modal isOpen onClose={onClose} isCentered preserveScrollBarGap>
-            <ModalOverlay />
-            <ModalContent maxW={{ base: "90%", sm: "450px" }}>
-              <ModalHeader>Unknown Error</ModalHeader>
-              <ModalBody>
-                <Center>
-                  <WarningTwoIcon boxSize={16} color="red.500" />
-                </Center>
-              </ModalBody>
-              <ModalFooter>
-                Unfortunately, an unexpected error has occurred. Please reload
-                the page to try again.
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-        )}
-        onError={console.error}
-      >
-        <NextNProgress height={5} />
-        <Component {...pageProps} />
-      </ErrorBoundary>
-    </ChakraProvider>
+    <ConvexProviderWithAuth0 client={convex} authInfo={authInfo}>
+      <ChakraProvider theme={theme}>
+        <ErrorBoundary
+          FallbackComponent={() => (
+            <Modal isOpen onClose={onClose} isCentered preserveScrollBarGap>
+              <ModalOverlay />
+              <ModalContent maxW={{ base: "90%", sm: "450px" }}>
+                <ModalHeader>Unknown Error</ModalHeader>
+                <ModalBody>
+                  <Center>
+                    <WarningTwoIcon boxSize={16} color="red.500" />
+                  </Center>
+                </ModalBody>
+                <ModalFooter>
+                  Unfortunately, an unexpected error has occurred. Please reload
+                  the page to try again.
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          )}
+          onError={console.error}
+        >
+          <NextNProgress height={5} />
+          <Component {...pageProps} />
+        </ErrorBoundary>
+      </ChakraProvider>
+    </ConvexProviderWithAuth0>
   );
 }
